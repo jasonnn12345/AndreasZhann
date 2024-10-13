d22536d680da04d3b9828d2e2184f6f667d0fc2e
AndreasZhann <andreaszhan4@gmail.com>
2024-10-13 21:26:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function keAtas(){
+    return location.href="#home-container"
+}
+function keTime(){
+    return location.href="#time-container"
+}
+function keweb(){
+    location.href ="https://www.google.com/maps/search/JW+Marriott/@3.5966489,98.6744508,18z?entry=ttu&g_ep=EgoyMDI0MTAwOS4wIKXMDSoASAFQAw%3D%3D"
+}
